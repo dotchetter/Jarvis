d15560d6936fc20468158ec097d7b4f972a99b7f
Simon Olofsson <dotchetter@protonmail.ch>
2021-10-13 21:20:15 +0200

    Improved GetExpenses intent
    author can be mentioned using @ from discord. This allows
    users to query expenses for other people than themselves.

--- jarvis/abilities/finances/ability.py
+++ jarvis/abilities/finances/ability.py
@@ -30,35 +30,48 @@ class AddExpense(Intent):
         if None in (expense_value, expense_name):
             return Reply("Du måste ange både namn och pris på vad du har köpt.")
 
-        if authors := Author.objects(name=message.author).all_fields():
-            author = authors.first()
-            print("This author is already in the database:", author)
-        else:
-            print("The author was not in the database; saving them")
-            author = Author(name=message.author).save()
-
-        expense = Expense(price=expense_value, name=expense_name, author=author)
+        expense = Expense(price=expense_value, name=expense_name, author=message.author.id)
         expense.save()
 
         return Reply(f"Utgift sparad: '{expense.name}', pris: {expense.price}:-")
 
 
-class GetExpensesForUser(Intent):
+class GetExpenses(Intent):
     """
     Returns a ReplyStream of all expenses for the
     user making the request.
+
+    If the user does not provide a name for someone
+    else which they would like to see their expenses
+    for; the query is performed on their name by
+    message.author.name.
     """
     lead = ("visa", "lista", "show", "get")
     trail = ("utgifter", "expenses", "expense", "utlägg")
 
     def respond(self, message: Message) -> Union[Reply, ReplyStream]:
-        author_expenses = Expense.objects(author__name=message.author)
-        return ReplyStream(f"Namn: {i.name}, pris: {i.price}" for i in author_expenses)
+        pretty_expenses = []
+
+        # Perform the query on the mentioned user, if present, else the author
+        if message.mentions:
+            author_id = message.mentions.pop().id
+        else:
+            author_id = message.author.id
+
+        try:
+            if author_expenses := Expense.objects(author=author_id):
+                for expense in author_expenses:
+                    pretty_expenses.append(f"* Namn: {expense.name}\n"
+                                           f"  Pris: {expense.price}\n"
+                                           f"  Datum: {expense.created}")
+                    return ReplyStream(pretty_expenses)
+        except pymongo.errors.ServerSelectionTimeoutError:
+            return Reply("Ett fel uppstod när data hämtades. Prova en annan sökning.")
+        return Reply(f"Det finns inga utgifter sparade för {message.author.mention}")
 
 
 class FinanceAbility(Ability):
-    intents = (AddExpense, GetExpensesForUser)
+    intents = (AddExpense, GetExpenses)
 
     def configure(self):
-        mongoengine.connect(db="jarvis", username="db_full_access_user",
-                            password=settings.MONGO_ATLAS_PW, host=settings.MONGO_ATLAS_URL)
+        mongoengine.connect(settings.MONGO_ATLAS_URL)
