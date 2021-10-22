0741ae39e9db4a78ffa3bfe4a0c0d500096bf38a
Simon Olofsson <dotchetter@me.com>
2021-10-22 18:36:35 +0200

    new intent for creating users

---
+++ jarvis/abilities/administrative/ability.py
@@ -0,0 +1,21 @@
+
+"""
+This ability file holds intents related to
+administrative intents in Jarvis.
+
+Administrative intents include creating
+database entries, changing settings, etc.
+"""
+from pyttman.core.intent import Intent
+
+
+class CreateUserIntent(Intent):
+    """
+    The intent creates a database User instance
+    for the provided data in message.author.
+
+    Since platforms represent users differently,
+    this uses whichever unique identifier provided
+    by the platform and associates the identifier
+    with the user.
+    """
