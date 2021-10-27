ca3b1e4db808cf88cc13191c6de07756faecaaf5
Simon Olofsson <dotchetter@me.com>
2021-10-27 17:31:21 +0200

    Models moved to a separate models file under the /administrative abiliy directory.

--- jarvis/abilities/finances/models.py
+++ jarvis/abilities/finances/models.py
@@ -1,9 +1,8 @@
 from datetime import datetime
 
 import mongoengine
-import uuid as uuid
 
-from mongoengine import Document, EmbeddedDocument, EmbeddedDocumentListField
+from mongoengine import Document, EmbeddedDocument
 
 
 class Expense(Document):
@@ -38,31 +37,3 @@ class ShoppingList(Document):
     a shopping list.
     """
     ingredients = mongoengine.ListField(mongoengine.EmbeddedDocumentField(Ingredient))
-
-
-class PlatformAuthorAlias(EmbeddedDocument):
-    """
-    The PlatformAuthorAlias model represents Authors
-    in Pyttman apps which vary depending on the
-    platform the app is connected to through
-    Clients.
-    """
-    uuid = mongoengine.StringField(primary_key=True)
-    name = mongoengine.DynamicField()
-
-
-class User(Document):
-    """
-    A platform-independent User for the Jarvis
-    application.
-
-    The User maps to author id to whichever
-    platforms are used with the app.
-
-    Users map One-to-many to PlatformAuthorAlias
-    model objects, to represent one user
-    on many platforms.
-    """
-    uuid = mongoengine.StringField(default=uuid.uuid4(), primary_key=True)
-    name = mongoengine.StringField()
-    platform_author_references = EmbeddedDocumentListField(PlatformAuthorAlias)
