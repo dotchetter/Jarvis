8ab422ad7b1517ee190852de19dece6af3eb54e9
Simon Olofsson <dotchetter@me.com>
2021-10-27 17:30:41 +0200

    Refactoring
    * Merged two intents to minimize code and keep the code dry.
      Instead of using two simliar intents, an Entity is parsed
      to identify whether or not to return the sum of all expenses.
    
    * Added rules for the 'lead' tuple
    
    * Renamed intents to improve readability and code clarity
    
    * Removed redundant helper method

--- jarvis/abilities/finances/ability.py
+++ jarvis/abilities/finances/ability.py
@@ -1,19 +1,18 @@
-import datetime
+import sys
+from datetime import datetime, timedelta
 from typing import Union
 
 import mongoengine
-import pymongo.errors
-import pyttman
 from pyttman import settings
 from pyttman.core.ability import Ability
 from pyttman.core.communication.models.containers import Reply, ReplyStream, Message
 from pyttman.core.intent import Intent
 from pyttman.core.parsing import identifiers
-from pyttman.core.parsing.identifiers import CapitalizedIdentifier
 from pyttman.core.parsing.parsers import ValueParser, ChoiceParser
 
 from jarvis.abilities.finances.month import Month
-from jarvis.models import Expense
+from jarvis.abilities.finances.util import get_message_author_id
+from jarvis.abilities.finances.models import Expense
 
 
 class AddExpenseIntent(Intent):
@@ -46,7 +45,7 @@ class AddExpenseIntent(Intent):
         return Reply(f"Utgift sparad-: '{expense.name}', pris: {expense.price}:-")
 
 
-class ListExpensesIntent(Intent):
+class GetExpensesIntent(Intent):
     """
     Returns a ReplyStream of all expenses for the
     user making the request.
@@ -56,59 +55,74 @@ class ListExpensesIntent(Intent):
     for; the query is performed on their name by
     message.author.name.
     """
-    lead = ("visa", "lista", "show", "get")
-    trail = ("utgifter", "expenses", "expense", "utlägg")
+    lead = ("visa", "lista", "show", "get", "hämta")
+    trail = ("utgift", "utgifter", "expense", "expenses", "utlägg")
     description = "Hämta utgifter för dig, eller en annan person." \
                   "Om du vill visa utgifter för någon annan, tagga " \
                   "dem med '@'."
     example = "Visa utgifter för @Simon"
 
-    def respond(self, message: Message) -> Union[Reply, ReplyStream]:
-        pretty_expenses = []
-        author_id = get_message_author_id(message)
-
-        try:
-            for expense in Expense.objects(author=author_id):
-                pretty_expenses.append(f":pinched_fingers: **{expense.name}**\n"
-                                       f":money_with_wings: {expense.price}:-\n"
-                                       f":calendar: **{expense.created.strftime('%y-%m-%d')}**")
-                pretty_expenses.append("=" * 20)
-        except pymongo.errors.ServerSelectionTimeoutError:
-            return Reply("Ett fel uppstod när data hämtades. "
-                         "Prova en annan sökning.")
-
-        if len(pretty_expenses):
-            return ReplyStream(pretty_expenses)
-        return Reply(f"Det finns inga utgifter sparade för den personen.")
-
-
-class SummarizeExpensesIntent(Intent):
-    """
-    Returns the sum of all expenses for the author.
-    """
-    description = "Visa summan för dig denna månad."
-    example = "Summera utlägg"
-    lead = ("summera", "sum", "summa")
-    trail = ("utlägg", "expenses", "purchase",
-             "purchases", "utgift", "utgifter")
-
     class EntityParser:
+        """
+        Provide users the ability to get the sum of their expenses,
+        and for which month the query is for.
+        """
+        sum_expenses = ChoiceParser(choices=("sum", "summa", "summera", "summerade",
+                                             "summed", "totalt", "totala", "total"))
         month = ChoiceParser(choices=tuple(i.name for i in Month))
+        user_id = ValueParser(prefixes=("for", "för"))
 
     def respond(self, message: Message) -> Union[Reply, ReplyStream]:
+        """
+        Isolate for which user the intent is for.
+        If the sum is asked for, simply provide only the
+        sum - otherwise return all expenses for the
+        selected month.
+        :param message:
+        :return:
+        """
+        pretty_expenses = []
+
+        # Use the provided month from entities if provided - or default to current month.
+        query_year = datetime.now().year
 
         try:
-            current_month: int = Month[self.entities["month"]].value
+            query_month: int = Month[self.entities["month"]].value
         except KeyError:
-            current_month: int = datetime.datetime.now().month
+            query_month: int = datetime.now().month
 
-        author_id = get_message_author_id(message)
+        # Filter the query on the selected month, of current year
+        query_month_name = Month(query_month).name
+        query_datetime_from = datetime(year=query_year, month=query_month, day=1)
+        query_datetime_to = datetime(year=query_year, month=query_month + 1, day=1)
+        query_datetime_to -= timedelta(days=1)
 
-        if not (expenses_for_author := Expense.objects(author=author_id)):
-            return Reply("Det finns inga utgifter sparade för den användaren")
+        # Default to message.author.id unless provided as an entity
+        try:
+            if not (user_id := int(self.entities.get("user_id"))):
+                raise ValueError
+            else:
+                user_id = int(user_id)
+        except (ValueError, TypeError):
+            user_id = message.author.id
 
-        filtered = [i.price for i in expenses_for_author if i.created.month == current_month]
-        return Reply(f"Summan för {Month(current_month).name} är **{sum(filtered)}**:-")
+        result = Expense.objects.filter(author=user_id,
+                                       created__gte=query_datetime_from,
+                                       created__lte=query_datetime_to)
+
+        if not result:
+            return Reply(self.storage["default_replies"]["no_expenses_matched_criteria"])
+
+        # The user wanted a sum of their expenses
+        if self.entities.get("sum_expenses"):
+            return Reply(f"Summan för {query_month_name} är **{result.sum('price')}**:-")
+
+        for expense in result:
+            pretty_expenses.append(f":pinched_fingers: **{expense.name}**\n"
+                                   f":money_with_wings: {expense.price}:-\n"
+                                   f":calendar: **{expense.created.strftime('%y-%m-%d')}**")
+            pretty_expenses.append("-" * 20)
+        return ReplyStream(pretty_expenses)
 
 
 class CreateSplitBillsReport(Intent):
@@ -117,6 +131,10 @@ class CreateSplitBillsReport(Intent):
     """
     def respond(self, message: Message) -> Union[Reply, ReplyStream]:
         pass
+
+    def test(self, my_list: list[str, int]) -> None:
+        print(my_list)
+        return None
 
 
 class FinanceAbility(Ability):
@@ -128,27 +146,23 @@ class FinanceAbility(Ability):
     expenses at home, to make splitting bills fair
     and square.
     """
-    intents = (AddExpenseIntent, ListExpensesIntent, SummarizeExpensesIntent)
+    intents = (AddExpenseIntent, GetExpensesIntent)
 
     def configure(self):
         # Connect to the MongoDB Atlas database
+        if settings.DEV_MODE:
+            settings.MONGO_DB_CONFIG["db"] = settings.DB_NAME_DEV
+        else:
+            settings.MONGO_DB_CONFIG["db"] = settings.DB_NAME_PROD
+
         mongoengine.connect(**settings.MONGO_DB_CONFIG)
 
+        # Set up a default reply when no expenses are found
+        self.storage.put("default_replies",
+                         {"no_expenses_matched_criteria": "Det finns inga utgifter "
+                                                          "sparade med angivna kriterier"})
 
-def get_message_author_id(message: Message) -> str:
-    """
-    Helper function until Pyttman provides the .author
-    attribute as an Author object, platform independent.
-
-    :param message: Pyttman Message to parse
-    :return: str
-    """
-    # Perform the query on the mentioned user, if present, else the author
-    try:
-        if message.mentions:
-            author_id = message.mentions.pop().id
-        else:
-            author_id = message.author.id
-    except AttributeError:
-        author_id = message.author
-    return author_id
+        if settings.INTERACTIVE_SHELL is True:
+            import IPython
+            IPython.embed()
+            sys.exit(0)
