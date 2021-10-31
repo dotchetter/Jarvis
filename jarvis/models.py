33807836f06bda86af0ef51306e4d8146238745e
Simon Olofsson <dotchetter@protonmail.ch>
2021-10-31 11:07:17 +0100

    Added a User model
    The User model represents a traditional User object in the Jarvis
    application.
    
    Multiple platforms have various aliases which makes keeping track
    of users challenging.
    
    To combat this, the User model has the "aliases" field which allows
    one User to be represented by many different aliases. They can be
    nicknames or given names; or platform-identifiers such as the user-id
    from Discord.
    
    This makes it easy for users to mention another user simply by providing
    their name.

--- jarvis/models.py
+++ jarvis/models.py
@@ -1,2 +1,34 @@
+class User(Document):
+    """
+    A platform-independent User for the Jarvis
+    application.
+
+    :field name:
+        String, username of a user.
+    """
+    username = mongoengine.StringField(required=True)
+    aliases = mongoengine.ListField(mongoengine.DynamicField())
+
+    @staticmethod
+    def get_by_alias_or_username(alias_or_username: str) -> QuerySet:
+        """
+        Offers a simpler way to find a User by a string
+        which could either be an alias or the correct
+        username.
+        :param alias_or_username:
+        :return: QuerySet
+        :raise: ValueError, if no user is found by either username or alias
+        """
+        # Casefold and truncate any special characters
+        alias_or_username = Message(alias_or_username).sanitized_content().pop()
+        user_by_username = User.objects.filter(username=alias_or_username)
+        user_by_alias = User.objects.filter(aliases__icontains=alias_or_username)
+
+        # Always prioritize username since it's a direct lookup
+        if len(user_by_username):
+            return user_by_username
+        elif len(user_by_alias):
+            return user_by_alias
+        raise ValueError("No user matched query by username or alias")
     """
     output_date_format = "%y-%m-%d"
