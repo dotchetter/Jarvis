e17aef79e13a34ab502799562f274004d8e7b6da
Simon Olofsson <dotchetter@protonmail.ch>
2022-04-20 23:34:18 +0200

    moved db conf to settings.py

--- jarvis/app.py
+++ jarvis/app.py
@@ -1,29 +1,13 @@
-import os
-
-import certifi
 import mongoengine
 from pyttman import app
-from dotenv import load_dotenv
 
 
 @app.hooks.run("before_start")
 def setup():
 
-    load_dotenv()
     # Connect to the appropriate MongoDB Atlas database
-    mongo_db_config = {
-        "tlsCAFile": certifi.where(),
-        "db": None,  # Configured in Ability 'Configure' hook
-        "host": os.getenv("MONGO_DB_URL"),
-        "username": os.getenv("MONGO_DB_USER"),
-        "password": os.getenv("MONGO_DB_PASSWORD"),
-        "port": int(os.getenv("MONGO_DB_PORT"))
-    }
-
     if app.settings.DEV_MODE:
-        mongo_db_config["db"] = app.settings.DB_NAME_DEV
+        app.settings.DATABASE["db"] = app.settings.DB_NAME_DEV
     else:
-        mongo_db_config["db"] = app.settings.DB_NAME_PROD
-
-    mongoengine.connect(**mongo_db_config)
-
+        app.settings.DATABASE["db"] = app.settings.DB_NAME_PROD
+    mongoengine.connect(**app.settings.DATABASE)
