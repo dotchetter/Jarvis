03f877348e8e3de3550280b1cc791c72456bffe4
Simon Olofsson <dotchetter@protonmail.ch>
2022-04-20 23:52:17 +0200

    renamed hook method to anonymous

--- jarvis/app.py
+++ jarvis/app.py
@@ -3,8 +3,7 @@ from pyttman import app
 
 
 @app.hooks.run("before_start")
-def setup():
-
+def _():
     # Connect to the appropriate MongoDB Atlas database
     if app.settings.DEV_MODE:
         app.settings.DATABASE["db"] = app.settings.DB_NAME_DEV
