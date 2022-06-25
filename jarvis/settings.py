dd81459a9783131ff13ea16278de5388ddb206e4
Simon Olofsson <dotchetter@protonmail.ch>
2022-06-25 22:55:17 +0200

    Updated version tag to 1.5.2.3

--- jarvis/settings.py
+++ jarvis/settings.py
@@ -57,7 +57,7 @@ LOG_FILE_DIR = APP_BASE_DIR / Path("logs")
 LOG_TO_STDOUT = True
 
 APP_NAME = "jarvis"
-APP_VERSION = "1.2.5.1"
+APP_VERSION = "1.2.5.3"
 DATETIME_FORMAT = "%Y-%m-%d-%H:%M"
 
 TIME_ZONE = datetime.utcnow().astimezone().tzinfo
