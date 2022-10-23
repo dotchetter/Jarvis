f37bfd289fd50f2f295833631d19f916e12b4c0d
Simon Olofsson <dotchetter@protonmail.ch>
2022-10-23 22:41:39 +0200

    New functionality: Projects: Refactoring, extracting methods to Ability class Projects encapsulate WorkShifts, telling which project they belong to. This allows for parallell projects to take place, keeping workshifts in order.

--- jarvis/abilities/timekeeper/ability.py
+++ jarvis/abilities/timekeeper/ability.py
@@ -1,31 +1,27 @@
-from datetime import datetime, date
+
+from datetime import datetime
 from decimal import Decimal, ROUND_HALF_UP
 from typing import Sequence
 
 import pyttman
-from mongoengine import Q, QuerySet
 from pyttman import app
 from pyttman.core.ability import Ability
-from pyttman.core.containers import Message, Reply
+from pyttman.core.containers import Message, Reply, ReplyStream
 
-from jarvis.abilities.timekeeper.intents import (
-    StartStopWatch,
-    StopStopWatch,
-    GetWorkshift,
-    CreateWorkshiftsFromString
-)
-from jarvis.abilities.timekeeper.models import WorkShift
+import jarvis.abilities.timekeeper.intents as intents
+from jarvis.abilities.timekeeper.models import WorkShift, Project
 from jarvis.models import User
 
 
 class TimeKeeper(Ability):
-    intents = (StartStopWatch,
-               StopStopWatch,
-               GetWorkshift,
-               CreateWorkshiftsFromString)
-
-    def before_create(self):
-        pass #WorkShift.time_zone = app.settings.TIME_ZONE
+    intents = (intents.CreateWorkShift,
+               intents.StopWorkShift,
+               intents.GetWorkshift,
+               intents.CreateWorkshiftsFromString,
+               intents.CreateNewProject,
+               intents.SetProjectAsDefault,
+               intents.ActivateProject,
+               intents.DeactivateProject)
 
     @staticmethod
     def get_total_billable_hours(*workshifts: Sequence[WorkShift]) -> Decimal:
@@ -42,59 +38,10 @@ class TimeKeeper(Ability):
         return Decimal(billable_hours).quantize(Decimal('.00'),
                                                 rounding=ROUND_HALF_UP)
 
-    @classmethod
-    def get_workshifts_for_current_month(cls, user: User) -> list[WorkShift]:
+    def save_workshift_from_string(self, message: Message):
         """
-        Get all workshifts for a user, which are all recorded in the
-        current month and have been consumed,
-        :param user: User owning the WorkShifts
+        Save a historic workshift from datetime or timestamps
         """
-        return WorkShift.objects.filter(
-            user=user,
-            month=datetime.now().month,
-            year=datetime.now().year
-        ).all()
-
-    @classmethod
-    def get_workshifts_for_today(cls, user: User) -> list[WorkShift]:
-        """
-        Get all workshifts for a user, which are all recorded in the
-        current month and have been consumed,
-        :param user: User owning the WorkShifts
-        """
-        return WorkShift.objects.filter(
-            user=user,
-            month=datetime.now().month,
-            year=datetime.now().year,
-            day=datetime.now().day
-        ).all()
-
-    @classmethod
-    def get_work_shifts_between_dates(cls, start_date: date,
-                                      end_date: date) -> QuerySet:
-        """
-        Returns WorkShift objects which were created in a datetime span
-        inclusively, and is consumed, meaning they're not active.
-
-        :param start_date: The oldest day for WorkShifts to be included
-        :param end_date: The most recent day for WorkShifts to be included
-        """
-        return WorkShift.objects.filter(year=start_date.year,
-                                        end__lte=end_date,
-                                        is_consumed=True)
-
-    @staticmethod
-    def get_currently_active_workshift(user: User) -> WorkShift | None:
-        """
-        Returns the currently active workshift for a User,
-        if there is any.
-        """
-        return WorkShift.objects.filter(
-            Q(user=user) & Q(is_active=True)
-        ).first()
-
-    @staticmethod
-    def save_workshift_from_string(message: Message):
         current_user = User.objects.from_message(message)
 
         # If from->to was entered as datetime, e.g. the shift didn't occur today
@@ -104,8 +51,16 @@ class TimeKeeper(Ability):
         # If from->to was entered as timestamps
         from_timestamp = message.entities["from_timestamp"]
         to_timestamp = message.entities["to_timestamp"]
+        project_name = message.entities["project_name"]
 
-        if not ((from_datetime and to_datetime) or (from_timestamp and to_timestamp)):
+        if (project := Project.objects.get_by_name_or_default_project(
+                project_name)) is None:
+            return self.complain_no_project_was_chosen()
+
+        workshifts_entered_as_datetime = from_datetime and to_datetime
+        workshifts_entered_as_time = from_timestamp and to_timestamp
+
+        if not (workshifts_entered_as_datetime or workshifts_entered_as_time):
             return Reply("Ange när arbetspasset började och slutade. "
                          "Om båda dessa infaller idag, räcker det med "
                          "klockslag, t.ex. `08:30 till 12:00`.")
@@ -143,10 +98,161 @@ class TimeKeeper(Ability):
                                  year=start_datetime.year,
                                  day=start_datetime.day,
                                  month=start_datetime.month,
+                                 project=project,
                                  manually_created=True)
 
         output_start = start_datetime.strftime(app.settings.DATETIME_FORMAT)
         output_end = end_datetime.strftime(app.settings.DATETIME_FORMAT)
 
-        return Reply("OK! Jag har sparat ett arbetspass från "
-                     f"{output_start} till {output_end} :slight_smile:")
+        return Reply(f"OK! Jag har sparat ett arbetspass med projekt {project} "
+                     f"från {output_start} till {output_end} :slight_smile:")
+
+    @classmethod
+    def create_new_project(cls, message: Message) -> Reply:
+        """
+        Create a new project in Jarvis, to register WorkShift objects for.
+        """
+        if (project_name := message.entities["project_name"]) is None:
+            return Reply("Du måste ange ett projektnamn.")
+        if Project.objects(name=project_name.casefold()).first():
+            return Reply("Det finns redan ett projekt med det namnet.")
+        project = Project.objects.create(name=project_name.casefold())
+        return Reply(f"Grattis till ditt nya projekt: '{project}'!")
+
+    @classmethod
+    def stop_current_workshift(cls, message: Message) -> Reply:
+        """
+        Stop the currently active workshift, for user
+        """
+        if (current_user := User.objects.from_message(message)) is None:
+            return Reply("Jag vet inte vem frågan gäller?")
+        if (shift := WorkShift.objects.get_active_shift_for_user(
+                current_user)) is None:
+            return Reply("Jag hittade inget aktivt arbetspass att avsluta")
+        shift.stop()
+        return Reply(f"Arbetspass avslutat. "
+                     f"Passet varade {shift.duration.hour} timmar, "
+                     f"{shift.duration.minute} minuter och "
+                     f"{shift.duration.second} sekunder.")
+
+    def get_worked_hours(self, message: Message) -> Reply:
+        """
+        Get how much the user has worked from a message,
+        based on whether to sum for today or for the current month.
+        """
+        project_name = message.entities["project_name"]
+
+        if (project := Project.objects.get_by_name_or_default_project(
+                project_name)) is None:
+            return Reply("Du har inte angivit något giltigt projekt, och "
+                         "det fanns inget standardprojekt att välja. "
+                         "Du kan välja mellan "
+                         f"{', '.join(Project.all_project_names())}.")
+
+        base_reply_string = "Totalt har du jobbat in {} timmar {} i projekt {}"
+        current_user = User.objects.from_message(message)
+        sum_for_today = message.entities["sum_for_today"]
+        sum_for_month = message.entities["sum_for_month"]
+
+        if not any((sum_for_month, sum_for_today)):
+            if (active_shift := WorkShift.objects.get_active_shift_for_user(
+                    current_user)) is None:
+                return Reply("Du har inget aktivt arbetspass")
+
+            shift_duration = active_shift.duration
+            return Reply("Du har ett aktivt arbetspass som pågått "
+                         f"{shift_duration.hour} timmar, "
+                         f"{shift_duration.minute} minuter och "
+                         f"{shift_duration.second} sekunder.")
+
+        if message.entities["sum_for_today"]:
+            shifts = WorkShift.objects.get_for_today_for_user_and_project(
+                user=current_user,
+                project=project)
+            hours = self.get_total_billable_hours(*shifts)
+            base_reply_string = base_reply_string.format(hours, "idag",
+                                                         project)
+        elif message.entities["sum_for_month"]:
+            shifts = WorkShift.objects.get_all_for_user_in_current_month(
+                user=current_user,
+                project=project)
+            hours = self.get_total_billable_hours(*shifts)
+            base_reply_string = base_reply_string.format(hours, "denna månad",
+                                                         project)
+        return Reply(base_reply_string)
+
+    @classmethod
+    def set_project_as_default(cls, message: Message) -> Reply:
+        """
+        Assign a Project to be the 'default' project, as a fallback
+        for intents where project is required but not mentioned
+        explicitly.
+        """
+        project_name = message.entities["project_name"]
+
+        if (project_by_name := Project.objects.try_get(name=project_name)) is None:
+            return Reply("Jag hittade inte det projektet, "
+                         "kontrollera stavningen.")
+
+        if project_by_name.is_active is False:
+            return Reply("Projektet kan inte användas som "
+                         "standardprojekt eftersom det inte är aktivt.")
+
+        for project in Project.objects.all():
+            if project == project_by_name:
+                project.is_default = True
+            else:
+                project.is_default = False
+            project.save()
+
+        return Reply(f"Jag har uppdaterat att projekt {project_name} "
+                     f"är standardprojektet från och med nu. Arbetspass "
+                     f"som skapas utan ett projektnamn, kommer automatiskt "
+                     f"att bokföras för detta projektet.")
+
+    @classmethod
+    def get_project_by_name(cls, project_name: str) -> Project | Reply:
+        """
+        Get a project from name or a reply mentioning no Project was found
+        """
+        if (project := Project.objects.try_get(name=project_name)) is None:
+            return Reply("Jag hittade inte det projektet, "
+                         "kontrollera stavningen.")
+        return project
+
+    @classmethod
+    def activate_project(cls, message: Message) -> Reply:
+        """
+        Activate a project
+        """
+        project_name = message.entities["project_name"]
+        project: Project | Reply = cls.get_project_by_name(project_name)
+        project.is_active = True
+        project.save()
+        return Reply(f"Projekt {project.name} är nu aktivt.")
+
+    @classmethod
+    def deactivate_project(cls, message: Message) -> Reply:
+        """
+        Deactivate a project
+        """
+        project_name = message.entities["project_name"]
+        project: Project | Reply = cls.get_project_by_name(project_name)
+        project.is_active = False
+        project.save()
+        return Reply(f"Projekt {project.name} är nu inaktivt.")
+
+    @classmethod
+    def complain_no_project_was_chosen(cls) -> ReplyStream:
+        """
+        Replies with a message telling that no project is active,
+        and no fallback default is selected.
+        """
+        reply_stream = ReplyStream()
+        available_projects = (str(i) for i in Project.objects.get_active())
+        reply_stream.put("Du har inte angivit något giltigt projekt, "
+                         "och det fanns inget standardprojekt att välja.")
+        if available_projects:
+            reply_stream.put("Du kan välja mellan "
+                             f"{', '.join(available_projects)}.")
+        return reply_stream
--- jarvis/abilities/timekeeper/intents.py
+++ jarvis/abilities/timekeeper/intents.py
@@ -1,37 +1,35 @@
-from datetime import datetime
-from decimal import Decimal
-
-import pyttman
-from mongoengine import Q
-from pyttman import app
 from pyttman.core.containers import ReplyStream, Reply, Message
 from pyttman.core.entity_parsing.fields import BoolEntityField, \
     StringEntityField
 from pyttman.core.entity_parsing.identifiers import DateTimeStringIdentifier
 from pyttman.core.intent import Intent
 
-from jarvis.abilities.timekeeper.models import WorkShift
+from jarvis.abilities.timekeeper.models import WorkShift, Project
 from jarvis.custom_identifiers import TimeStampIdentifier
 from jarvis.models import User
 
 
-class StartStopWatch(Intent):
+class CreateWorkShift(Intent):
     """
     Starts a WorkShift or an Intermission.
     """
     lead = ("starta", "börja", "börjar", "påbörja", "ta", "tagit", "tar")
     trail = ("arbetspass", "pass", "skift", "jobb", "jobba", "arbeta")
+    project_name = StringEntityField(valid_strings=Project.all_project_names)
 
     def respond(self, message: Message) -> Reply | ReplyStream:
+        project_name = message.entities["project_name"]
+
         if (current_user := User.objects.from_message(message)) is None:
             return Reply("Jag vet inte vem frågan gäller?")
-
         if complaint := self.complain_if_workshift_exists(current_user):
             return complaint
+        if (project := Project.objects.get_by_name_or_default_project(
+                project_name)) is None:
+            return self.ability.complain_no_project_was_chosen()
 
-        workshift = WorkShift.objects.create(user=current_user)
-        workshift.start()
-        return Reply("Okej, jag har noterat att skift har startats")
+        WorkShift.objects.create(user=current_user, project=project).start()
+        return Reply(f"Ett skift startades för projektet {project}.")
 
     @staticmethod
     def complain_if_workshift_exists(user: User) -> Reply | None:
@@ -39,19 +37,16 @@ class StartStopWatch(Intent):
         Returns a Reply if the user already has an active WorkShift,
         else None.
         """
-        active_shift = WorkShift.objects.filter(
-            Q(user=user) & Q(is_active=True)
-        ).first()
-
-        if active_shift:
+        if active_shift := WorkShift.objects.get_active_shift_for_user(user):
             return Reply("Du är redan i ett aktivt arbetspass som "
                          f"påbörjades {active_shift.beginning} och har pågått "
                          f"{active_shift.duration.hour} timmar, "
                          f"{active_shift.duration.minute} minuter och "
-                         f"{active_shift.duration.second} sekunder.")
+                         f"{active_shift.duration.second} sekunder, på "
+                         f"projekt {active_shift.project.name}.")
 
 
-class StopStopWatch(Intent):
+class StopWorkShift(Intent):
     """
     Ends a current WorkShift or Intermission.
     """
@@ -62,20 +57,7 @@ class StopStopWatch(Intent):
              "jag")
 
     def respond(self, message: Message) -> Reply | ReplyStream:
-        if (current_user := User.objects.from_message(message)) is None:
-            return Reply("Jag vet inte vem frågan gäller?")
-
-        shift: WorkShift = WorkShift.objects.filter(
-            Q(user=current_user) & Q(is_active=True)
-        ).first()
-
-        if shift is None:
-            return Reply("Jag hittade inget aktivt arbetspass att avsluta")
-        shift.stop()
-        return Reply(f"Arbetspass avslutat. "
-                     f"Passet varade {shift.duration.hour} timmar, "
-                     f"{shift.duration.minute} minuter och "
-                     f"{shift.duration.second} sekunder.")
+        return self.ability.stop_current_workshift(message)
 
 
 class GetWorkshift(Intent):
@@ -88,43 +70,76 @@ class GetWorkshift(Intent):
     sum_for_today = BoolEntityField(message_contains=("idag", "idag?"))
     sum_for_month = BoolEntityField(message_contains=("månad", "månaden",
                                                       "månad?", "månaden?"))
+    project_name = StringEntityField(valid_strings=Project.all_project_names)
 
     def respond(self, message: Message) -> Reply | ReplyStream:
-        base_reply_string = "Totalt har du jobbat in {} timmar {}"
-        current_user = User.objects.from_message(message)
-        sum_for_today = message.entities["sum_for_today"]
-        sum_for_month = message.entities["sum_for_month"]
-        if not any((sum_for_month, sum_for_today)):
-            if (active_shift := self.ability.get_currently_active_workshift(
-                    current_user)) is None:
-                return Reply("Du har inget aktivt arbetspass")
-            shift_duration = active_shift.duration
-            return Reply("Du har ett aktivt arbetspass som pågått "
-                         f"{shift_duration.hour} timmar, "
-                         f"{shift_duration.minute} minuter och "
-                         f"{shift_duration.second} sekunder.")
-
-        if message.entities["sum_for_today"] is True:
-            shifts = self.ability.get_workshifts_for_today(current_user)
-            hours = self.ability.get_total_billable_hours(*shifts)
-            base_reply_string = base_reply_string.format(hours, "idag")
-        elif message.entities["sum_for_month"] is True:
-            shifts = self.ability.get_workshifts_for_current_month(current_user)
-            hours = self.ability.get_total_billable_hours(*shifts)
-            base_reply_string = base_reply_string.format(hours, "denna månad")
-        return Reply(base_reply_string)
+        return self.ability.get_worked_hours(message)
 
 
 class CreateWorkshiftsFromString(Intent):
-
+    """
+    Store a workshift by entering timestamp or datetime stamps manually.
+    """
+    help_string = __doc__
     lead = ("lägg", "spara", "skapa", "nytt")
     trail = ("pass", "arbetspass", "skift", "timmar")
 
     from_datetime = StringEntityField(identifier=DateTimeStringIdentifier)
     to_datetime = StringEntityField(identifier=DateTimeStringIdentifier)
-
     from_timestamp = StringEntityField(identifier=TimeStampIdentifier)
     to_timestamp = StringEntityField(identifier=TimeStampIdentifier)
+    project_name = StringEntityField(valid_strings=Project.all_project_names)
 
     def respond(self, message: Message) -> Reply | ReplyStream:
         return self.ability.save_workshift_from_string(message)
+
+
+class CreateNewProject(Intent):
+    """
+    Create a new Project, to store workshifts for
+    """
+    project_name = StringEntityField(span=5)
+    lead = ("skapa", "nytt")
+    trail = ("projekt",)
+
+    def respond(self, message: Message) -> Reply | ReplyStream:
+        return self.ability.create_new_project(message)
+
+
+class SetProjectAsDefault(Intent):
+    """
+    Set a project as default; meaning it's implicitly selected if
+    the project name is omitted when a workshift is created.
+    """
+    lead = ("projekt", "sätt")
+    trail = ("default", "standard", "standardprojekt")
+    project_name = StringEntityField(valid_strings=Project.all_project_names)
+
+    def respond(self, message: Message) -> Reply | ReplyStream:
+        return self.ability.set_project_as_default(message)
+
+
+class ActivateProject(Intent):
+    """
+    Activate a project in Jarvis
+    """
+    lead = ("aktivera",)
+    trail = ("projekt",)
+    project_name = StringEntityField(valid_strings=Project.all_project_names,
+                                     span=5)
+
+    def respond(self, message: Message) -> Reply | ReplyStream:
+        return self.ability.activate_project(message)
+
+
+class DeactivateProject(Intent):
+    """
+    Deactivate a project in Jarvis
+    """
+    lead = ("avaktivera", "deaktivera", "pensionera", "inaktivera")
+    trail = ("projekt",)
+    project_name = StringEntityField(valid_strings=Project.all_project_names,
+                                     span=5)
+
+    def respond(self, message: Message) -> Reply | ReplyStream:
+        return self.ability.deactivate_project(message)
